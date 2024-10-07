1f0aebf458b032c78a2407b3b6aab563c88884fc
Harsimrat Bajwa <harsimrat713@gmail.com>
2024-10-06 21:33:53 -0400

    Updated logs and added key to each child

--- src/logBook.js
+++ src/logBook.js
@@ -2,20 +2,54 @@
 // just using log object here for now
 const logs = [
   {
+    key: "1",
     dateAccomplished: "2024-09-16",
     workDone: "Created Website",
-    membersParticipated: ["Russell"],
+    membersParticipated: "Russell",
   },
   {
+    key: "2",
     dateAccomplished: "2024-09-17",
     workDone:
       "Researched avalible professors from MME, ECE, BME, SYDE, and Optomitry to see who could possibly help us and provide more information.",
-    membersParticipated: ["All Team members"],
+    membersParticipated: "All Team members",
   },
   {
+    key: "3",
     dateAccomplished: "2024-09-20",
     workDone: "Updated website to use react and implemnted a log book",
-    membersParticipated: ["Harsimrat"],
+    membersParticipated: "Harsimrat",
+  },
+  {
+    key: "4",
+    dateAccomplished: "2024-09-27",
+    workDone:
+      "Researched and came up with 4 possible solutions as well as 3 Patents",
+    membersParticipated: "All Team members",
+  },
+  {
+    key: "5",
+    dateAccomplished: "2024-10-4",
+    workDone: "Started research into AI models and tech required for project",
+    membersParticipated: "Russell",
+  },
+  {
+    key: "6",
+    dateAccomplished: "2024-10-6",
+    workDone: "Had a in person meeting to discuss best solution",
+    membersParticipated: "All Team members",
+  },
+  {
+    key: "7",
+    dateAccomplished: "2024-10-6",
+    workDone: "Reached out to professors in school of optomitry",
+    membersParticipated: "Selvin",
+  },
+  {
+    key: "8",
+    dateAccomplished: "2024-10-6",
+    workDone: "Started to apply to Awards",
+    membersParticipated: "Timothy",
   },
 ];
 
@@ -23,7 +57,7 @@ const logs = [
 const LogEntries = ({ logs }) => (
   <div className="Log-Book-Entries">
     {logs.map((log) => (
-      <div className="Log-Book-Entry">
+      <div className="Log-Book-Entry" key={log.key}>
         <p className="Log-Book-Entry-Date">{log.dateAccomplished}</p>
         <p className="Log-Book-Entry-Content">{log.workDone}</p>
         <p className="Log-Book-Entry-participents">
